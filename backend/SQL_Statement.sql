1b27d56e77f1aec1d868b1f9203e535696836b97
William F Nicholson <William-N@users.noreply.github.com>
2020-03-30 23:39:55 -0400

    Added table to database
    Incorporated database table in order to facilitate events that'll replace the placeholders on the web page.

--- backend/SQL_Statement.sql
+++ backend/SQL_Statement.sql
@@ -6,5 +6,17 @@ CREATE TABLE Persons (
     Email varchar(255) NOT NULL,
     Profile_Pic varchar(255),
     PreferGame varchar(255),
-    EventList varchar(255)
+);
+
+CREATE TABLE Events (
+    ID INTEGER NOT NULL PRIMARY KEY,
+    EventImage VARCHAR(255),
+    Title VARCHAR(255) NOT NULL,
+    Description VARCHAR(255)
+);
+
+CREATE TABLE Persons_Events (
+    ID INTEGER NOT NULL PRIMARY KEY,
+    UserID INTEGER NOT NULL,
+    EventID INTEGER NOT NULL
 );
